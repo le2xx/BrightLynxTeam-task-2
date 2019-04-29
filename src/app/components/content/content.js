6983f9434ed4f1f8d4ca92f25853f9fc9641b688
le2xx <le2x@rambler.ru>
2019-04-29 16:46:43 +0300

    Created function checkInput Added horizontal and vertical points arrays

--- src/app/components/content/content.js
+++ src/app/components/content/content.js
@@ -1,4 +1,30 @@
 const content = () => {
-	console.log('content');
+	const form = document.getElementById('form');
+	const input = form.querySelector('.content__input');
+	const button = form.querySelector('.content__btn');
+
+	const horizontSteps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+	const verticalSteps = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+	const checkInput = str => { // проверка корретности вве
+		const inPosition = str.split('');
+
+		const lenCheck = (str.length === 2);
+		const hCheck = horizontSteps.includes(inPosition[0]) || false;
+		const vCheck = verticalSteps.includes(inPosition[1]) || false;
+
+		return lenCheck && hCheck && vCheck;
+	};
+
+	const horseSteps = () => {
+		const startPoint = input.value.toUpperCase();
+		if (!checkInput(startPoint)) {
+			alert('На шахматной доске нет ячейки: ' + startPoint + ', введите правильное значение');
+			return;
+		}
+		alert('Возможные варианты хода: ' + checkInput(startPoint));
+	};
+
+	button.addEventListener('click', horseSteps);
 };
 export { content };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './styles/base.styl';
+import {content} from "./app/components/content/content";
 // import { testBlock } from './app/components/test-block/test-block';
 
 window.onload = () => {
   // testBlock();
+  content();
 };
